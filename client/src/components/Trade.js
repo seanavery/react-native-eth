3e071fb66312924c14d5f2858c7b69838a1c3d19
Sean Pollock <seanpollock3344@gmail.com>
2017-03-06 20:59:35 -0600

    Touchable highlights over buttons

--- client/src/components/Trade.js
+++ client/src/components/Trade.js
@@ -8,44 +8,54 @@ import {
 import { Router } from '../../index.ios.js';
 
 export default class Trade extends Component {
+  handleSell() {
 
+  }
 
+  handleBuy() {
+
+  }
   render() {
     return (
       <View style={{flex: 1, flexDirection: 'column'}}>
-        <View style={{width: '100%', height: '50%', backgroundColor: 'lightgrey'}}>
-          <View style={{
-            flex: 1,
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}>
-            <Text style={{
+        <TouchableHighlight onPress={this.handleBuy}>
+          <View style={{width: '100%', height: '50%', backgroundColor: 'lightgrey'}}>
+            <View style={{
               flex: 1,
-              fontFamily: 'Helvetica',
-              fontWeight: 'bold',
-              fontSize: 50,
-              textAlign: 'center',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center'
             }}>
-              BUY UE
-            </Text>
+              <Text style={{
+                flex: 1,
+                fontFamily: 'Helvetica',
+                fontWeight: 'bold',
+                fontSize: 50,
+                textAlign: 'center',
+              }}>
+                BUY UE
+              </Text>
+            </View>
           </View>
-        </View>
-        <View style={{width: '100%', height: '50%', backgroundColor: 'darkgrey'}}>
-          <View style={{flex: 1,
-            flexDirection: 'row',
-            alignItems: 'center'}}>
-            <Text style={{
-              flex: 1,
-              fontFamily: 'Helvetica',
-              fontWeight: 'bold',
-              fontSize: 50,
-              textAlign: 'center'
-            }}>
-              SELL UE
-            </Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={this.handleSell}>
+          <View style={{width: '100%', height: '50%', backgroundColor: 'darkgrey'}}>
+            <View style={{flex: 1,
+              flexDirection: 'row',
+              alignItems: 'center'}}>
+              <Text style={{
+                flex: 1,
+                fontFamily: 'Helvetica',
+                fontWeight: 'bold',
+                fontSize: 50,
+                textAlign: 'center'
+              }}>
+                SELL UE
+              </Text>
+            </View>
           </View>
-        </View>
+        </TouchableHighlight>
       </View>
     )
   }

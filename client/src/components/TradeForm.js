ae5026f162f3bbc6a34482a9368519460beb42ff
Sean Pollock <seanpollock3344@gmail.com>
2017-03-07 13:31:19 -0600

    Styling TradeForm and putting in slider

--- client/src/components/TradeForm.js
+++ client/src/components/TradeForm.js
@@ -15,10 +15,20 @@ export default class TradeForm extends Component {
 
   render() {
     return (
-      <View>
-        <Text>
-        afadf
-        </Text>
+      <View style={{flex: 1, flexDirection: 'column'}}>
+        <View style={{width: '100%', height: '33%', }}>
+          <Text>
+            20 UE
+          </Text>
+        </View>
+        <View style={{width: '100%', height: '33%', }}>
+          <Text>
+            3.45 GBP
+          </Text>
+        </View>
+        <View style={{width: '100%', height: '33%', }}>
+          <Slider />
+        </View>
       </View>
     )
   }

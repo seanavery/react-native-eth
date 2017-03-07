83201017eaf5ed5a6d20aab23922533f643dd081
Sean Pollock <seanpollock3344@gmail.com>
2017-03-06 18:28:11 -0600

    Making flexbox with row

--- client/src/components/Trade.js
+++ client/src/components/Trade.js
@@ -7,7 +7,9 @@ import {
 export default class Trade extends Component {
   render() {
     return (
-      <View>
+      <View style={{flex: 2, flexDirection: 'column'}}>
+        <View style={{width: '100%', height: '50%', backgroundColor: 'lightgrey'}} />
+        <View style={{width: '100%', height: '50%', backgroundColor: 'darkgrey'}} />
       </View>
     )
   }

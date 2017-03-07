a36ace2614d4b0b360bffcf2a8ac65ca01b410c1
Sean Pollock <seanpollock3344@gmail.com>
2017-03-07 14:51:54 -0600

    Succesfully hit my resful api using fetch

--- client/src/components/TradeForm.js
+++ client/src/components/TradeForm.js
@@ -6,32 +6,61 @@ import {
   Button,
 } from 'react-native';
 
-import { Router } from '../../index.ios.js';
+// import { Router } from '../../index.ios.js';
 
 export default class TradeForm extends Component {
-  state = {
-    gbp_amount: 0,
-    ue_amount: 0,
+  constructor(props) {
+    super(props);
+    this.state = {
+      ue_amount: 0
+    }
+    this.handleOrder = this.handleOrder.bind(this);
+  }
+
+  handleOrder() {
+    console.log('this.state is', this.state)
+    fetch('http://localhost:6000/submitBuy', {
+      method: 'Post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'applicatoin/json',
+      },
+      body: JSON.stringify({
+        ue: this.state.ex
+      })
+    });
   }
 
   render() {
     return (
       <View style={{flex: 1, flexDirection: 'column', backgroundColor: 'lightgrey'}}>
         <View style={{width: '100%', height: '33%', flex: 1, flexDirection: 'row', alignItems: 'center'}}>
-          <View>
+          <View style={{flex: 1}}>
+            <Text style ={{
+              textAlign: 'center',
+              color: 'darkgrey',
+              fontSize: 50,
+              fontFamily: 'Helvetica'
+            }}>
+              20 UE
+            </Text>
           </View>
-          <Text style ={{textAlign: 'center'}}>
-            20 UE
-          </Text>
         </View>
         <View style={{width: '100%', height: '33%', flex: 1, flexDirection: 'row', alignItems: 'center'}}>
-          <Text>
-            3.45 GBP
-          </Text>
+          <View style={{flex: 1}}>
+            <Text style ={{
+              textAlign: 'center',
+              color: 'darkgrey',
+              fontSize: 50,
+              fontFamily: 'Helvetica'
+            }}>
+              3.45 GBP
+            </Text>
+          </View>
         </View>
         <View style={{width: '100%', height: '33%', }}>
           <Slider />
-          <Button title="submit order" style={{backgroundColor: 'darkgrey', flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+          <Button onPress={this.handleOrder}title="submit order" style={{backgroundColor: 'darkgrey', flex: 1, flexDirection: 'row', alignItems: 'center'}}>
           </Button>
         </View>
       </View>

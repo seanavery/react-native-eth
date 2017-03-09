769efaa44280f556e75e44563d5ab263f22ba592
Sean Pollock <seanpollock3344@gmail.com>
2017-03-09 16:13:36 -0600

    Promise chain to hit /approve ande /sell

--- server/restful-api/tests/client.js
+++ server/restful-api/tests/client.js
@@ -5,16 +5,26 @@ import request from 'request';
   unit tests for client facing API
 */
 
-var sell = {
+const sell = {
   method: 'POST',
   uri: 'http://localhost:6000/sell',
   body: {},
   json: true
 }
 
-var approve = {
+const approve = {
   method: 'POST',
   uri: 'http://localhost:6000/approve',
   body: {},
   json: true
 }
+
+Proise.delay(0)
+.then(() => {
+  return rp(sell);
+}).then((body) => {
+  console.log('/sell ==>', body);
+  return rp(approve);
+}).then((body) => {
+  console.log('/approve ==>', body);
+})

b4bed69fdad6b181b452564b6316cca004672a0a
Sean Pollock <seanpollock3344@gmail.com>
2017-03-09 15:37:27 -0600

    drafting URI schema for restful interface

--- server/restful-api/server.js
+++ server/restful-api/server.js
@@ -1,22 +1,51 @@
 import express from 'express';
 import Promise from 'bluebird';
-import orderbook from '../order-book/orderbook';
 import redis from 'redis';
 
-// create a redis client
+/*
+  REDIS CLIENT
+  listens on port 6379 by default
+  createClient(_host_, _port_)
+*/
 const client = redis.createClient();
-console.log('redis client', client)
 
-// client.on('error', (err) => {
-//   console.log('Error', err);
-// })
+// listen for errors from redis client
+client.on('error', (err) => {
+  console.log('Error from Redis client', err);
+})
 
+/*
+  EXPRESS SERVER
+  http interface
+  GET, POST, PUT, DELETE
+*/
 const app = express();
 
-const options = {};
-let market;
 
-app.post('/submitBuy', (req, res) => {
+/*
+====================================================
+  CLIENT INTERFACE
+
+  applications, trading bots, etc...
+
+  POST:
+
+  POST URI:
+  /approve/:token_contract_address;
+  /transfer/:token_contract_address;
+  /submitSell/:token1_contract_address/:token2_contract_address
+
+  GET:
+
+  GET URI:
+  /approve/:token_contract_address/:seller_address
+  /submitSell/:token_contract_addres/:seller_address
+
+====================================================
+*/
+
+// POST
+app.post('/submitSell', (req, res) => {
   Promise.delay()
   .then(() => {
     // call approve on erc20 token contract
@@ -25,6 +54,19 @@ app.post('/submitBuy', (req, res) => {
   })
 });
 
+
+
+/*
+====================================================
+  ADMIN Interface
+
+  server based exchange agents, Consenergy admins
+
+  POST:
+  /match/:exchange_contract/
+
+====================================================
+*/
 Promise.delay(0)
 .then(() => {
   market = new orderbook(options)
@@ -34,3 +76,4 @@ Promise.delay(0)
 app.listen(6000);
 
 console.log('API listening on port 6000');
+console.log('Redis client on port 6379');

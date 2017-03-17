9f26986af07e8028dcd74b8365cbaac4c6c719b0
Sean Pollock <seanpollock3344@gmail.com>
2017-03-17 11:25:05 -0500

    Starting graphing library.

---
+++ client/src/components/graphs/Pie.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+// import * as d3 from 'd3';
+
+var data = [
+    {"number":  8, "name": 'Fun activities'},
+    {"number": 7, "name": 'Dog'},
+    {"number": 16, "name": 'Food'},
+    {"number": 23, "name": 'Car'},
+    {"number": 42, "name": 'Rent'},
+    {"number":  4, "name": 'Misc'},
+  ];
+
+export default class Pie extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View width=300 height=300>
+        <Text>Pie Chart </Text>
+      </View>
+    );
+  }
+}

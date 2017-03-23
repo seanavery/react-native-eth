bd5d66f49bf0e3fd487595cb8865080d51279e34
Sean Pollock <seanpollock3344@gmail.com>
2017-03-23 12:34:58 -0500

    Restructuring main process loop.

--- server/gauss-sim/time_series_generator.js
+++ server/gauss-sim/time_series_generator.js
@@ -1,30 +1,68 @@
 import Promise from 'bluebird';
 
 // globals
+let starting_price = .5;
+let starting_variance = .2
+
+// recursive simulation process
+
+function simulationLoop() {
+  return Promise.delay(5000)
+  .then(() => {
+    console.log('trading event');
+    return abc();
+  }).then(() => {
+    return simulationLoop();
+  }).catch((error) => {
+    console.log('error', error);
+    Promise.delay(5000)
+    .then(() => {
+      return simulationLoop();
+    })
+  })
+}
+
+// calculate normal random
+export function bellRandom(mean) {
+  conosole.log('calculating normal random');
+  return mean;
+
+  return new Promise(resolve, rejct)
+}
+
+simulationLoop();
+
+
+
 
 // instantiate process
-Promise.delay(0)
-.then(() => {
-  runSimulation()''
-}).catch((error) => {
-  console.log(error);
-})
-
-function runSimulation() {
-  return Promise.delay(3000)
-  .then(() => {
-    return marketPrice();
-  }).then((price) => {
-    console.log(price);
-    return runSimulation();
-  }).catch((error) => {
-    return Promise.delay(3000)
-    .then(() => { return runSimulation();})
-  })
-}
-
-marketPrice() {
-  return new Promise((resolve, reject) => {
-    Promise.resolve(normalRandom(market_price, 1))
-  })
-}
+// Promise.delay(0)
+// .then(() => {
+//   runSimulation();
+// }).catch((error) => {
+//   console.log(error);
+// });
+//
+// function runSimulation() {
+//   return Promise.delay(3000)
+//   .then(() => {
+//     return marketPrice();
+//   }).then((price) => {
+//     console.log('price', price);
+//     console.log('date', new Date());
+//     return runSimulation();
+//   }).catch((error) => {
+//     return Promise.delay(3000)
+//     .then(() => { return runSimulation();})
+//   })
+// }
+//
+// const marketPrice = () => {
+//   return new Promise((resolve, reject) => {
+//     Promise.resolve(normalRandom(market_price, 1))
+//   })
+// }
+//
+// const normalRandom = () => {
+//
+// }

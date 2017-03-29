0698c9e8f90b25ba86c33e6e60b1340f58e73415
Sean Pollock <seanpollock3344@gmail.com>
2017-03-29 10:13:34 -0500

    Simple React compnent utilizing react-native art library.

--- client/src/components/graphs/LineChart.js
+++ client/src/components/graphs/LineChart.js
@@ -1,44 +1,23 @@
 import React, { Component } from 'react';
-import {
-  View,
-  ART
-} from 'react-native';
+
+import { ART } from 'react-native';
 
 const {
-  Surface,
   Group,
-  Shape
+  Shape,
+  Surface,
 } = ART;
 
-import mock_data from './graph-utils/mock-data';
-import { createLineGraph } from './graph-utils/line-graph-lib.js';
-
-export default class LineChart extends Component {
-  constructor(props) {
-    super(props)
-  }
-
+class LineChart extends Component {
   render() {
-    const size = 300;
-    const data = createLineGraph({
-      data: mock_data,
-      width: size,
-      height: size,
-      xAccessor: d => new Date(d.time*1000),
-      yAccessor: d => d.temperatureMax
-    })
-    return(
-      <View height={300} width={300}>
-        <Surface width={300} height={300}>
-          <Group x={0} y={0}>
-            <Shape
-              d={data.path}
-              stroke="#000"
-              strokeWidth={1}
-            />
-          </Group>
-        </Surface>
-      </View>
-    )
+    <Surface width={200} height={100}>
+      <Group x={0} y={0}>
+        <Shape
+          d={this.props.linePath}
+          stroke="#000"
+          strokeWidth={1}
+        />
+      </Group>
+    </Surface>
   }
 }
